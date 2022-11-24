2ade0a58cea2e06b5b98609a9ad22b5f3e0c220e
Rian Rahman <rian.rahman@gatech.edu>
2022-11-23 20:48:28 -0500

    Code refactored

--- index.js
+++ index.js
@@ -66,41 +66,6 @@ const keys = {
     }
 }
 
-// Determines if a player's attack box has collided with the other player
-function rectangularCollision({rectangle1, rectangle2}) {
-    return (
-        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x 
-        && rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width
-        && rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y
-        && rectangle1.attackBox.position.y <= rectangle2.position.y + enemy.height
-    )
-}
-
-function determineWinner({player, enemy, timerId}) {
-    clearTimeout(timerId)
-    if (player.health === enemy.health) {
-        document.getElementById("gameOver").innerHTML = "Tie";
-    } else if (player.health > enemy.health) {
-        document.getElementById("gameOver").innerHTML = "Player 1 Wins";
-    } else if (player.health < enemy.health) {
-        document.getElementById("gameOver").innerHTML = "Player 2 Wins";
-    }
-    document.getElementById("gameOver").style.display = "flex";
-}
-
-let timer = 10;
-let timerId;
-function decreaseTimer() {
-    timerId = setTimeout(decreaseTimer, 1000);
-    if (timer > 0) {
-        timer--;
-        document.getElementById("timer").innerHTML = timer;
-    }
-    if (timer === 0) {
-        determineWinner({player, enemy, timerId});
-    }
-}
-
 decreaseTimer();
 
 // Loops canvas frames for animation
